a858b91e7861111970c7931ab97bfa501b1378b4
Luk Lambrechts <luk@Luks-MacBook-Pro.local>
2023-06-03 12:12:41 +0200

    t49:49 add ssrprovider from bootstrap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./globals.css";
 import { Inter } from "next/font/google";
-import { Container } from "@/components/bootstrap";
+import { Container, SSRProvider } from "@/components/bootstrap";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +18,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Container>{children}</Container>
+        <SSRProvider>
+          <main>
+            <Container className="py-4">{children}</Container>
+          </main>
+        </SSRProvider>
       </body>
     </html>
   );
