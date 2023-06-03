de0adacda89f807bbfbf3e3d262e104456befd40
Luk Lambrechts <luk@Luks-MacBook-Pro.local>
2023-06-03 12:07:14 +0200

    t47:37 make it possible to pass server components into a clientcomponent

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,25 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { Container } from "@/components/bootstrap";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'NextJS 13.4 Image Gallery',
-  description: 'Tutorial project by coding in flow',
-}
+  title: "NextJS 13.4 Image Gallery",
+  description: "Tutorial project by coding in flow",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div>This div is ahared across layouts</div>
-        {children}</body>
+        <Container>{children}</Container>
+      </body>
     </html>
-  )
+  );
 }
---
+++ src/components/bootstrap.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export {
+    Container, SSRProvider, Alert, Spinner
+} from "react-bootstrap";
