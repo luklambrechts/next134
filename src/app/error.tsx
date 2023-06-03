b3c81c3802ff2a7dbf52bd526ee8975e0d1be961
Luk Lambrechts <luk@Luks-MacBook-Pro.local>
2023-06-03 11:40:13 +0200

    t42:36 styling and button is working

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,10 +1,18 @@
 "use client";
 
-export default function Error() {
+import { Button } from "react-bootstrap";
+
+interface ErrorPageProps {
+    error: Error,
+    reset: () => void
+}
+
+export default function Error({error, reset}: ErrorPageProps) {
     return(
         <div>
             <h1>Error !!!</h1>
             <p>Something went wrong</p>
+            <Button onClick={reset}>try again</Button>
         </div>
     )
 }
--- src/app/hello/page.tsx
+++ src/app/hello/page.tsx
@@ -2,6 +2,6 @@ import { resolve } from "path"
 
 export default async function Page() {
     await new Promise((resolve) => setTimeout(resolve, 1000))
-    throw Error("Basinga");
+    // throw Error("Basinga");
     return <div> Hello, Nextjs 13 !</div>
 }
