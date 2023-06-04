8eb30a69766bf1b1bb7bb0c3e40ae5b923351cfc
Luk Lambrechts <luk@Luks-MacBook-Pro.local>
2023-06-04 11:22:25 +0200

    t1:02:05 navbar

---
+++ src/app/NavBar.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Link from "next/link";
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+export default function NavBar() {
+  return (
+    <Navbar
+      bg="primary"
+      variant="dark"
+      sticky="top"
+      expand="sm"
+      collapseOnSelect
+    >
+      <Container>
+        <Navbar.Brand as={Link} href="/">
+          NextJS 13.4 Image Gallery
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="main-navbar"></Navbar.Toggle>
+        <Navbar.Collapse id="main-navbar">
+          <Nav>
+            <Nav.Link href="/hello">Hello </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
