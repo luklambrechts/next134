539df2830e378244ccd448001821f8f49eb0e833
Luk Lambrechts <luk@Luks-MacBook-Pro.local>
2023-06-06 15:01:56 +0200

    t1:29:39 loading an image

--- src/app/static/page.tsx
+++ src/app/static/page.tsx
@@ -1,5 +1,11 @@
 import { UnsplashImage } from "@/models/unsplash.image"
 import Image from "next/image"
+import Link from "next/link"
+import { Alert } from "@/components/bootstrap"
+
+export const metadata = {
+    title: "Static fetching - nextjs 13.4 image gallery",
+  };
 
 export default async function Page() {
     console.log(process.env.UNSPLASH_ACCESS_KEY)
@@ -14,12 +20,19 @@ export default async function Page() {
 
     return (
         <div className="d-flex flex-column align-items-center">
+            <Alert>
+                This page <strong> fetches and caches at buildtime. </strong>
+                Even though the unsplash api always return a new image, we see the same image after refreshing the page until we compile the project again
+            </Alert>
             <Image
                 src={image.urls.raw}
                 width={width}
                 height={height}
-                alt={image.description}                
+                alt={image.description}
+                className="rounded shadow mw-100 h-100"                
             />
+            by <Link href={"/users/" + image.user.username}>{image.user.username}</Link> 
+            
         </div>
     )
 }
