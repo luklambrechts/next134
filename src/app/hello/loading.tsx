533fc064012a536e6d3316f58cee7cde7d2f2b59
Luk Lambrechts <luk@Luks-MacBook-Pro.local>
2023-06-07 12:40:58 +0200

    delete hello page

--- src/app/hello/loading.tsx
+++
@@ -1,7 +0,0 @@
-import { Spinner } from "@/components/bootstrap";
-
-export default function Loading() {
-    return(
-        <Spinner animation="border" className="d-block m-auto"></Spinner>
-    )
-}
--- src/app/hello/page.tsx
+++
@@ -1,7 +0,0 @@
-import { resolve } from "path"
-
-export default async function Page() {
-    await new Promise((resolve) => setTimeout(resolve, 1000))
-    // throw Error("Basinga");
-    return <div> Hello, Nextjs 13 !</div>
-}
