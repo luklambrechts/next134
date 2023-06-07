8eeabe8592d28da20e19a7b66b9a8504111474df
Luk Lambrechts <luk@Luks-MacBook-Pro.local>
2023-06-07 21:58:39 +0200

    t1:55:53 error >50 ?

--- src/app/(SSR)/topics/[topic]/page.tsx
+++ src/app/(SSR)/topics/[topic]/page.tsx
@@ -7,6 +7,12 @@ interface PageProps {
   // searchParams: { [key: string]: string | string[] | undefined },
 }
 
+export function generateStaticParams() {
+    return [
+       "health", "fitness", "coding"
+    ].map(topic => ({ topic }));
+}
+
 export default async function Page({ params: { topic } }: PageProps) {
   const response = await fetch(
     `https://api.unsplash.com/photos/random?query=${topic}&count=8&client_id=${process.env.UNSPLASH_ACCESS_KEY}`
