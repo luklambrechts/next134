86a6a2c21d2ea62e38b0fba5a2fbf4b859b46f6a
Luk Lambrechts <luk@Luks-MacBook-Pro.local>
2023-06-07 21:40:33 +0200

    t1:51:54 creating lists

--- src/app/(SSR)/topics/[topic]/page.tsx
+++ src/app/(SSR)/topics/[topic]/page.tsx
@@ -1,29 +1,31 @@
-import { UnsplashImage } from "@/models/unsplash.image"
+import { UnsplashImage } from "@/models/unsplash.image";
+import Image from "next/image";
+import styles from "./TopicPage.module.css"
 
 interface PageProps {
-    params: { topic: string},
-    // searchParams: { [key: string]: string | string[] | undefined },
+  params: { topic: string };
+  // searchParams: { [key: string]: string | string[] | undefined },
 }
 
-export default async function Page( { params: { topic } }: PageProps ) {
+export default async function Page({ params: { topic } }: PageProps) {
+  const response = await fetch(
+    `https://api.unsplash.com/photos/random?query=${topic}&count=8&client_id=${process.env.UNSPLASH_ACCESS_KEY}`
+  );
+  const images: UnsplashImage[] = await response.json();
 
-    const response = await fetch(`https://api.unsplash.com/photos/random?query=${topic}&count=8&client_id=${process.env.UNSPLASH_ACCESS_KEY}`)
-    const images: UnsplashImage[] = await response.json()    
-
-    return(
-        <div>
-        <h1>{topic}</h1>
-        {
-            images.map(image => {
-            <Image
-                src={image.urls.raw}
-                width={250}
-                height={250}
-                alt={image.description}
-                key={image.urls.raw}
-            ></Image>
-        })
-    
-        </div>
-    )
+  return (
+    <div>
+      <h1>Topic: {topic}</h1>
+      {images.map((image) => (
+        <Image
+          src={image.urls.raw}
+          width={250}
+          height={250}
+          alt={image.description}
+          key={image.urls.raw}
+          className={styles.image}
+        />
+      ))}
+    </div>
+  );
 }
