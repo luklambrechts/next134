c12e0205625d30b70b8a82946f1a4d5c6e85282c
Luk Lambrechts <luk@Luks-MacBook-Pro.local>
2023-06-07 13:06:58 +0200

    t1:34:57 ssr without cache

---
+++ src/app/(SSR)/dynamic/page.tsx
@@ -0,0 +1,37 @@
+import { UnsplashImage } from "@/models/unsplash.image"
+import Image from "next/image"
+import Link from "next/link"
+import { Alert } from "@/components/bootstrap"
+
+export const metadata = {
+    title: "Dynamic fetching - nextjs 13.4 image gallery",
+  };
+
+export const revalidate = 0;
+export default async function Page() {
+    console.log(process.env.UNSPLASH_ACCESS_KEY)
+    const fetchpath = `https://api.unsplash.com/photos/random?client_id=${process.env.UNSPLASH_ACCESS_KEY}`
+    console.log("fetchpath")
+    console.log(fetchpath);
+    const response = await fetch(fetchpath)
+    const image: UnsplashImage = await response.json()
+
+    const width = Math.min(image.width, 500)
+    const height = (width /image.width) * image.height
+
+    return(
+        <div className="d-flex flex-column align-items-center">
+        <Alert>
+        </Alert>
+        <Image
+            src={image.urls.raw}
+            width={width}
+            height={height}
+            alt={image.description}
+            className="rounded shadow mw-100 h-100"                
+        />
+        by <Link href={"/users/" + image.user.username}>{image.user.username}</Link> 
+        
+    </div>
+    );
+}
--- src/app/(SSR)/static/page.tsx
+++ src/app/(SSR)/static/page.tsx
@@ -12,7 +12,7 @@ export default async function Page() {
     const fetchpath = `https://api.unsplash.com/photos/random?client_id=${process.env.UNSPLASH_ACCESS_KEY}`
     console.log("fetchpath")
     console.log(fetchpath);
-    const response = await fetch(fetchpath)
+    const response = await fetch(fetchpath, { cache: "no-cache" })
     const image: UnsplashImage = await response.json()
 
     const width = Math.min(image.width, 500)
