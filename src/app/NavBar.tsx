71cc2a4e17fb860d8fb8128081a3a1cde804b71f
Luk Lambrechts <luk@Luks-MacBook-Pro.local>
2023-06-09 18:27:09 +0200

    add menu for dynamic items

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
 import { usePathname } from "next/navigation"
 
 export default function NavBar() {
@@ -25,6 +25,11 @@ export default function NavBar() {
             <Nav.Link as={Link} href="/static" active={pathname === "/static"}>Static</Nav.Link>
             <Nav.Link as={Link} href="/dynamic" active={pathname === "/dynamic"}>Dynamic</Nav.Link>
             <Nav.Link as={Link} href="/isr" active={pathname === "/isr"}>isr</Nav.Link>
+            <NavDropdown title="Topics" id="topics-dropdown">
+              <NavDropdown.Item as={Link} href="/topics/health">Health</NavDropdown.Item>
+              <NavDropdown.Item as={Link} href="/topics/fitness">Fitness</NavDropdown.Item>
+              <NavDropdown.Item as={Link} href="/topics/coding">Coding</NavDropdown.Item>
+            </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </Container>
