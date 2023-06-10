27bf6bfb3f78d8fcd0558bf557577c22f321c71e
Luk Lambrechts <luk@Luks-MacBook-Pro.local>
2023-06-10 19:20:29 +0200

    t2:23:45 create search-dialogue

--- src/app/(CSR)/search/SearchPage.tsx
+++ src/app/(CSR)/search/SearchPage.tsx
@@ -1,5 +1,26 @@
 "use client";
 
+import { FormEvent } from "react";
+import { Button, Form } from "react-bootstrap";
+
 export default function SearchPage() {
-    return <div>search</div>
+    async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        const formData = new FormData(e.target as HTMLFormElement);
+        const query = formData.get("query")?.toString().trim();
+        if (query) {
+            alert(query);
+        }
+    }
+    return <div>
+        <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="searh-input">
+                <Form.Label>Search query</Form.Label>
+                <Form.Control
+                 name="query"
+                 placeholder="E.g. cats, hotdogs, ..." />
+            </Form.Group>
+            <Button type="submit" className="mb-3">Search</Button>
+        </Form>
+    </div>
 }
