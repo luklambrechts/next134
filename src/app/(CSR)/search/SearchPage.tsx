1807296112c4554ecb040d51611ad957387f3c2e
Luk Lambrechts <luk@Luks-MacBook-Pro.local>
2023-06-12 16:21:05 +0200

    t2:44:21 search page is working

--- src/app/(CSR)/search/SearchPage.tsx
+++ src/app/(CSR)/search/SearchPage.tsx
@@ -2,48 +2,73 @@
 
 import { UnsplashImage } from "@/models/unsplash_image";
 import { FormEvent, useState } from "react";
-import { Button, Form, Spinner } from "react-bootstrap";
+import { Alert, Button, Form, Spinner } from "react-bootstrap";
+import Image from "next/image";
+import styles from "./SearchPage.module.css";
 
 export default function SearchPage() {
-    const [searchResults, setSearchResults] = useState<UnsplashImage[] | null>(null);
-    const [searchResultsloading, setSearchResultsLoading] = useState(false);
-    const [searchResultsLoadingIsError, setSearchResultsLoadingIsError] = useState(false);
-    async function handleSubmit(e: FormEvent<HTMLFormElement>) {
-        e.preventDefault();
-        const formData = new FormData(e.target as HTMLFormElement);
-        const query = formData.get("query")?.toString().trim();
-        if (query) {
-            try {
-                setSearchResults(null);
-                setSearchResultsLoading(false);
-                setSearchResultsLoading(true);
-                const response = await fetch(`/api/search?query=${query}`);
-                const images: UnsplashImage[] = await response.json();
-                setSearchResults(images);
-            } catch (error) {
-                    console.error(error);
-                    setSearchResultsLoadingIsError(true);
-            } finally {
-                setSearchResultsLoading(false);
-            }
-           
-        }
+  const [searchResults, setSearchResults] = useState<UnsplashImage[] | null>(
+    null
+  );
+  const [searchResultsloading, setSearchResultsLoading] = useState(false);
+  const [searchResultsLoadingIsError, setSearchResultsLoadingIsError] =
+    useState(false);
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const query = formData.get("query")?.toString().trim();
+    if (query) {
+      try {
+        setSearchResults(null);
+        setSearchResultsLoading(false);
+        setSearchResultsLoading(true);
+        const response = await fetch(`/api/search?query=${query}`);
+        const images: UnsplashImage[] = await response.json();
+        setSearchResults(images);
+      } catch (error) {
+        console.error(error);
+        setSearchResultsLoadingIsError(true);
+      } finally {
+        setSearchResultsLoading(false);
+      }
     }
-    return <div>
-        <Form onSubmit={handleSubmit}>
-            <Form.Group className="mb-3" controlId="searh-input">
-                <Form.Label>Search query</Form.Label>
-                <Form.Control
-                 name="query"
-                 placeholder="E.g. cats, hotdogs, ..." />
-            </Form.Group>
-            <Button type="submit" className="mb-3">Search</Button>
-        </Form>
-        <div className="d-flex flex-column align-items-center">
-            {searchResultsloading && <Spinner animation="border" />}
-            {searchResultsLoadingIsError && <div>Something went wrong. Please try again.</div>}
-            {searchResults?.length === 0 && <p>Nothing found. Try a different query!</p>}
-        </div>
-        
+  }
+  return (
+    <div>
+        <Alert>This pages fetches data client-side. In order to not leak API credentials, the request is set to a NextJS route handler that runs on the server. This route handler then fetches the data from the Unsplash API and returns it to the client.</Alert>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="searh-input">
+          <Form.Label>Search query</Form.Label>
+          <Form.Control name="query" placeholder="E.g. cats, hotdogs, ..." />
+        </Form.Group>
+        <Button type="submit" className="mb-3" disabled={searchResultsloading}>
+          Search
+        </Button>
+      </Form>
+      <div className="d-flex flex-column align-items-center">
+        {searchResultsloading && <Spinner animation="border" />}
+        {searchResultsLoadingIsError && (
+          <div>Something went wrong. Please try again.</div>
+        )}
+        {searchResults?.length === 0 && (
+          <p>Nothing found. Try a different query!</p>
+        )}
+      </div>
+
+      {searchResults && (
+        <>
+          {searchResults.map((image) => (
+            <Image
+              src={image.urls.raw}
+              height={250}
+              width={250}
+              alt={image.description}
+              key={image.urls.raw}
+              className={styles.image}
+            ></Image>
+          ))}
+        </>
+      )}
     </div>
+  );
 }
